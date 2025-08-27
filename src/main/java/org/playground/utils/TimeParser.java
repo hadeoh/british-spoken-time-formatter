3fae5c03b8f519c098ec4e70234dba3d05245b1d
Usman Adio <usthmandanfodio@gmail.com>
2025-08-27 21:27:30 +0100

    Add Jacoco for code coverage and Github action

--- src/main/java/org/playground/utils/TimeParser.java
+++ src/main/java/org/playground/utils/TimeParser.java
@@ -11,10 +11,6 @@ public class TimeParser {
     public static LocalTime parse(String time) {
         // This is to validate the time format passed in the input e.g 11:25
         try {
-//            if (!time.matches("(^(?:[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$)?")) {
-//                throw new InvalidTimeFormatException("Time must be in HH:mm format: " + time);
-//            }
-
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("H:mm");
 
             return LocalTime.parse(time, formatter);
